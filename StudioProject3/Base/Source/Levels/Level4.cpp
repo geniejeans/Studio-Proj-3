65332204e651eac7d387db652a604391dedff0dd
Jean <lzyjeann@gmail.com>
2017-08-29 10:53:31 +0800

    one more file whoops

--- StudioProject3/Base/Source/Levels/Level4.cpp
+++ StudioProject3/Base/Source/Levels/Level4.cpp
@@ -144,7 +144,6 @@ void Level4::Init()
 
 
 	//	std::cout << _DEBUG << std::endl;
-	int a;
 
 	// Create and attach the camera to the scene
 	camera.Init(playerInfo->GetPos(), playerInfo->GetTarget(), playerInfo->GetUp());
@@ -178,8 +177,8 @@ void Level4::Init()
 	float halfFontSize = fontSize / 2.0f;
 	//Creating textOBj
 	textObj[0] = Create::Text2DObject("text", Vector3(-halfWindowWidth, halfWindowHeight - halfFontSize, 0.0f), "", Vector3(fontSize, fontSize, fontSize), Color(0.0f, 1.0f, 0.0f));
-	textObj[1] = Create::Text2DObject("text", Vector3(-halfWindowWidth + fontSize * 2, -halfWindowHeight + fontSize * 2.5, 0.1f), "", Vector3(fontSize, fontSize, fontSize), Color(0.0f, 1.0f, 0.0f));
-	textObj[3] = Create::Text2DObject("text", Vector3(0 - fontSize * 2, halfWindowHeight - fontSize, 0.1f), "", Vector3(fontSize, fontSize, fontSize), Color(0.0f, 1.0f, 0.0f));
+	textObj[1] = Create::Text2DObject("text", Vector3(-halfWindowWidth + fontSize * 2.f, -halfWindowHeight + fontSize * 2.5f, 0.1f), "", Vector3(fontSize, fontSize, fontSize), Color(0.0f, 1.0f, 0.0f));
+	textObj[3] = Create::Text2DObject("text", Vector3(0 - fontSize * 2.f, halfWindowHeight - fontSize, 0.1f), "", Vector3(fontSize, fontSize, fontSize), Color(0.0f, 1.0f, 0.0f));
 
 	//Minimap
 	theMinimap = Create::Minimap(false);
@@ -229,7 +228,7 @@ void Level4::Init()
 
 void Level4::Update(double dt)
 {
-	elapsed_time += dt;
+	elapsed_time += (float)dt;
 	
 	// Indicator RayCasting
 	if (!GameUI::GetInstance()->GetBombRender())
@@ -243,7 +242,7 @@ void Level4::Update(double dt)
 	}
 
 	// Money Rain
-	m_fTime_MoneyRain += dt;
+	m_fTime_MoneyRain += (float)dt;
 
 	if (m_bSwitchTime_MoneyRain)
 	{
@@ -267,7 +266,7 @@ void Level4::Update(double dt)
 	}
 
 	// Thunder Storm
-	m_fTime_ThunderStorm += dt;
+	m_fTime_ThunderStorm += (float)dt;
 
 	if (m_bSwitchTime_ThunderStorm)
 	{
@@ -292,16 +291,16 @@ void Level4::Update(double dt)
 
 	if (elapsed_time >= 3.f)
 	{
-		complete_time -= dt;
+		complete_time -= (float)dt;
 		if (complete_time <= 0)
 			complete_time = 0.0f;
 		//Hardware Abstraction
 		if (theKeyboard)
-			theKeyboard->Read(dt);
+			theKeyboard->Read((float)dt);
 
 		if (theMouse)
 		{
-			theMouse->Read(dt);
+			theMouse->Read((float)dt);
 			theMouse->SetTroopMovement(*BombTarget, *IndicatorTarget, test);
 		}
 		spawnDelay += (float)dt;
